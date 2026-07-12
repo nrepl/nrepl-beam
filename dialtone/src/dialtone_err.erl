b551469d712d0d9133ab2b26e1a1bc44e1ee7432
Bozhidar Batsov <bozhidar@batsov.dev>
2026-07-12 22:42:19 +0300

    Prune eval-machinery frames from reported stack traces
    Frames at or below erl_eval/dialtone_worker/dialtone_backend_* are
    noise to the person whose code raised - what they need stops at their
    own last frame. Mirrors what repartee already does on the Elixir side.

--- dialtone/src/dialtone_err.erl
+++ dialtone/src/dialtone_err.erl
@@ -20,9 +20,27 @@ render(BMod, Class, Reason, Stack, BState) ->
     end.
 
 default(Class, Reason, Stack) ->
-    Err = erl_error:format_exception(Class, Reason, Stack),
+    Err = erl_error:format_exception(Class, Reason, prune(Stack)),
     Ex = io_lib:format("~w:~0tP", [Class, Reason, 8]),
     #{err => Err, ex => Ex}.
+
+%% Frames at or below the eval machinery are noise to the person whose code
+%% raised: cut the trace at the first internal frame.
+prune(Stack) ->
+    case lists:splitwith(fun({Mod, _F, _A, _Loc}) ->
+                                 not internal(Mod)
+                         end, Stack) of
+        {[], _} -> Stack;
+        {UserFrames, _} -> UserFrames
+    end.
+
+internal(erl_eval) -> true;
+internal(dialtone_worker) -> true;
+internal(Mod) ->
+    case atom_to_binary(Mod, utf8) of
+        <<"dialtone_backend", _/binary>> -> true;
+        _ -> false
+    end.
 
 %% @doc Backend err_result (chardata) -> wire fields (utf8 binaries).
 -spec to_wire(dialtone_backend:err_result()) -> #{binary() => binary()}.
